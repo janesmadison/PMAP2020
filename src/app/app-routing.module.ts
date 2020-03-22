1ef1a137119e035db97b360e6b1a0bc63f0d0e9a
Madison Janes <madisonoliviajanes@gmail.com>
2020-03-22 19:57:20 -0400

    Cleaned up pages and routing. Introduced lazy loading modules..

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeNavComponent } from './home-nav/home-nav.component';
+import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
-import { CreateSurveyComponent } from './create-survey/create-survey.component';
-import { AddAdministratorComponent } from './add-administrator/add-administrator.component';
-import { MySurveysComponent } from './my-surveys/my-surveys.component';
-import { SurveyResultsComponent } from './survey-results/survey-results.component';
+import { CreateSurveyComponent } from './components/create-survey/create-survey.component';
+import { AddAdministratorComponent } from './components/add-administrator/add-administrator.component';
+import { MySurveysComponent } from './components/my-surveys/my-surveys.component';
+import { SurveyResultsComponent } from './components/survey-results/survey-results.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  {path: 'home', component:HomeNavComponent },
-  {path: 'create-survey', component:CreateSurveyComponent },
-  {path: 'add-administrator', component:AddAdministratorComponent },
-  {path: 'survey-results', component:SurveyResultsComponent},
-  {path: 'my-surveys',component:MySurveysComponent},
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [
+      //need an empty elements component
+      {path: 'create-survey', component: CreateSurveyComponent },
+      {path: 'add-administrator', component: AddAdministratorComponent },
+      {path: 'survey-results', component: SurveyResultsComponent },
+      {path: 'my-surveys',component: MySurveysComponent },
+    ]
+  },
+
   {path: '', redirectTo: '/login', pathMatch: 'full'},
-  {path: '**', redirectTo: '/login' }
 ];
 
 @NgModule({
