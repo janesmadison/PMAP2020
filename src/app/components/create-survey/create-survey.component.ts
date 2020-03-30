afdd8f657d09d72a011af6f993aa815b503b7e87
Madison Janes <madisonoliviajanes@gmail.com>
2020-03-30 13:41:18 -0400

    hopefully working

--- src/app/components/create-survey/create-survey.component.ts
+++ src/app/components/create-survey/create-survey.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {MatRadioModule} from '@angular/material/radio';
-
+import { ActivatedRoute } from '@angular/router';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators, FormControl, FormArray } from '@angular/forms';
-
+import { Student } from './student';
 import { Survey, Question } from '../../services/survey';
-
+import * as XLSX from 'xlsx';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -16,11 +16,18 @@ import { Subscription } from 'rxjs';
 })
 export class CreateSurveyComponent implements OnInit {
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private route: ActivatedRoute) {}
 
   questionForm: FormGroup;
   count = 1;
   sample: string;
+  emailList = 'Before';
+  testNum = 0;
+  emailArr = null;
+  cellIndex = 'C2';
+  workSheet = null;
+  nameArr = null;
+  students = [];
 
 
   ngOnInit() {
@@ -46,6 +53,89 @@ export class CreateSurveyComponent implements OnInit {
     this.count -= 1;
     this.questionsArr.removeAt(index);
   }
+
+  onFileChange(ev) {
+  let workBook = null;
+  let jsonData = null;
+
+  const reader = new FileReader();
+  const file = ev.target.files[0];
+
+  reader.onload = (event) => {
+    const data = reader.result;
+    workBook = XLSX.read(data, { type: 'binary' });
+    jsonData = workBook.SheetNames.reduce((initial, name) => {
+      const sheet = workBook.Sheets[name];
+      this.workSheet = workBook.Sheets[name];
+      initial[name] = XLSX.utils.sheet_to_json(sheet);
+      return initial;
+    }, {}); // end of .reduce
+
+    const dataString = JSON.stringify(jsonData);
+    // document.getElementById('output').innerHTML = dataString.slice(0, 30000).concat('...');
+
+
+    // this.IncrementCellRow(this.cellIndex);
+    // console.log(this.cellIndex);
+    this.parseNameColumn();
+    this.ParseEmailCell();
+    this.FillClassRoster();
+
+    if (this.workSheet['C12'] === undefined) { console.log('undef'); }
+    this.ParseEmailCell();
+  }; // end of onload
+  reader.readAsBinaryString(file);
+  }// end of onFileChange
+
+  IncrementCellColumn(cellIndex) {
+}// end of Incrememnt Cell Column
+
+IncrementCellRow(cellIndex) {
+console.log(this.cellIndex);
+const count = cellIndex.match(/\d*$/);
+
+this.cellIndex = this.cellIndex.substr(0, count.index) + (++count[0]);
+console.log(this.cellIndex);
+}// end of Increment Cell Row
+
+parseNameColumn() {
+var numBlankSpaces = 0;
+
+while (numBlankSpaces < 8) {
+  console.log(this.workSheet[this.cellIndex]);
+  if (this.workSheet[this.cellIndex] === undefined) {
+    numBlankSpaces++;
+  } else {
+    var cell = this.workSheet[this.cellIndex];
+    var cellData = (cell ? cell.v : undefined);
+    console.log('place the names in the queue ' + cellData );
+    console.log(cellData);
+    this.nameArr = this.nameArr + '*' + cellData; // gives a separator for the names
+    console.log(this.nameArr);
+    numBlankSpaces = 0;
+  }
+  this.IncrementCellRow(this.cellIndex);
+}
+
+this.nameArr = this.nameArr.split('*'); // splits by the separator to get an array of the names
+this.nameArr.splice(0, 1);
+console.log(this.nameArr);
+}
+
+ParseEmailCell() {
+var cell = this.workSheet['L1'];
+var cellData = (cell ? cell.v : undefined);
+this.emailArr = cellData;
+this.emailArr = this.emailArr.split(',');
+console.log(this.emailArr);
+// now add these to a class that has names and emails and concatenate them to display in the list card
+}// Parse Email Cell
+
+FillClassRoster() {
+  for (let i in this.nameArr) {
+  this.students.push(new Student(this.nameArr[i], this.emailArr[i]));
+  }
+}
 
 
 }
---
+++ src/app/components/create-survey/student.ts
@@ -0,0 +1,10 @@
+export class Student {
+name: string;
+email: string;
+
+  constructor(studentName: string, studentEmail: string) {
+  this.name = studentName;
+  this.email = studentEmail;
+  }
+
+}
