7378c0bd90e016647c8824c2fd165da5356bb39b
faithah11 <faith11hough@gmail.com>
2020-04-06 14:18:47 -0400

    merged with madisons changes. new error message and simpler validation logic

--- src/app/components/change-pass/change-pass.component.ts
+++ src/app/components/change-pass/change-pass.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder, FormGroupDirective, NgForm } from '@angular/forms';
 import { Observable, throwError } from 'rxjs';
+import {ErrorStateMatcher} from '@angular/material/core';
 
 import { Subscription } from 'rxjs';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    return control.dirty && form.invalid;
+  }
+}
 
 @Component({
   selector: 'app-change-pass',
@@ -14,10 +21,9 @@ import { Subscription } from 'rxjs';
 export class ChangePassComponent {
 
   changePassForm: FormGroup;
-  oldPass = new FormControl('', [Validators.required, Validators.minLength(6),
-    Validators.maxLength(50), Validators.pattern('[a-zA-Z0-9]*')]);
-  newPass = new FormControl('', [Validators.required, Validators.minLength(6)]);
-  confirmPass = new FormControl('', [Validators.required, Validators.minLength(6)]);
+  oldPass = new FormControl('', [Validators.required, Validators.minLength(6)]);
+  newPass = new FormControl('', [Validators.required, Validators.minLength(6), Validators.pattern('[a-zA-Z0-9]*')]);
+  confirmPass = new FormControl('', [Validators.required, Validators.minLength(6), Validators.pattern('[a-zA-Z0-9]*')]);
 
   constructor(
     private router: Router,
@@ -32,20 +38,11 @@ export class ChangePassComponent {
           });
         }
 
+        matcher = new MyErrorStateMatcher();
+
         passwordMatchValidator(g: FormGroup) {
          return g.get('newPass').value === g.get('confirmPass').value
             ? null : { mismatch: true };
-            // throwError('Passwords do not match!');
-        }
-        setOldPass() {
-          return { 'has-danger': !this.oldPass.pristine && !this.oldPass.valid };
-        }
-        setNewPass() {
-          return { 'has-danger': !this.newPass.pristine && !this.newPass.valid };
-        }
-        setConfirmPass() {
-          return { 'has-danger': !this.confirmPass.pristine && !this.confirmPass.valid ||
-           !this.confirmPass.pristine && this.changePassForm.errors && this.changePassForm.errors.mismatch};
         }
 
         changePass() {
