1c7346f983dc294e6d73edcf7884c774c471fd79
faithah11 <faith11hough@gmail.com>
2020-04-10 23:17:18 -0400

    added necessary changes from code review to create-survey

--- src/app/components/create-survey/create-survey.component.ts
+++ src/app/components/create-survey/create-survey.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import {MatRadioModule} from '@angular/material/radio';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, FormControl, FormArray } from '@angular/forms';
+import { Survey, Question } from '../../services/survey';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-create-survey',
@@ -8,10 +13,36 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CreateSurveyComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
-    
+  questionForm: FormGroup;
+  count = 1;
+  sample: string;
+
+//initializes a reactive form for the questions under a user specified title(survey name) it has a title and a question array
+  ngOnInit() {
+    this.questionForm = this.fb.group({
+      title: [],
+      questions: this.fb.array([this.fb.group({point: ''})])
+    });
   }
+
+//returns all of the questions that are in the current array
+  get questionsArr() {
+    return this.questionForm.get('questions') as FormArray;
+  }
+
+  // this function will push another question that the user inputs into the array
+  addQuestion() {
+    this.count += 1;
+    this.questionsArr.push(this.fb.group({point: ''}));
+  }
+
+// this function will take the most recently added question and delete it
+  deleteQuestion(index) {
+    this.count -= 1;
+    this.questionsArr.removeAt(index);
+  }
+
 
 }
