b81c8d4d5b9eda38c585f724697a03f69dcaf8a1
Madison Janes <madisonoliviajanes@gmail.com>
2020-04-20 14:00:59 -0400

    comment

--- backend/api/database.php
+++ backend/api/database.php
@@ -13,6 +13,7 @@ function connect()
   $connect = mysqli_connect(DB_HOST ,DB_USER ,DB_PASS ,DB_NAME);
 
   if (mysqli_connect_errno($connect)) {
+    echo 'failure';
     die("Failed to connect:" . mysqli_connect_error());
   }
 
