7dd90c0d217706d199d20650649ced29d0a5355f
Tavarius3398 <Tfleming1@students.fairmontstate.edu>
2020-04-20 10:26:03 -0400

    Administration email functionality added

--- backend/backendMailer.php
+++ backend/backendMailer.php
@@ -1,12 +1,29 @@
 <?php
-$to_email = "tfleming1@students.fairmontstate.edu";
-$subject = "Simple Email Test via PHP";
-$body = "Hi, This is test email send by PHP Script";
-$headers = "From: sender\'s email";
 
-if (mail($scope.newAdminEmail, $subject, $body, $headers)) {
-    echo "Email successfully sent to $to_email...";
-} else {
-    echo "Email sending failed...";
+switch($_SERVER['REQUEST_METHOD']){
+    case("OPTIONS"): //Allow preflighting to take place.
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Methods: POST");
+        header("Access-Control-Allow-Headers: content-type");
+        exit;
+    case("POST"): //Send the email;
+        header("Access-Control-Allow-Origin: *");
+
+        $json = file_get_contents('php://input');
+
+        $params = json_decode($json);
+        $email = $params->email;
+        $name = $params->name;
+
+        //$recipient = 'tfleming1@students.fairmontstate.edu';
+        $subject = 'PMAP2020 Administration invite';
+        $headers = "From: $name <$email>";
+        $message = 'You have been invited to join the PMAP2020 survey websites administration group! please follow the link to join! (LINK)';
+
+        mail($email, $subject, $message, $headers);
+        break;
+    default: //Reject any non POST or OPTIONS requests.
+        header("Allow: POST", true, 405);
+        exit;
 }
 ?>
