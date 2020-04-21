08106598a363c46e8b8913cfa313312f7e644db6
Tavarius3398 <Tfleming1@students.fairmontstate.edu>
2020-04-21 10:23:47 -0400

    Documentation update

--- src/app/components/add-administrator/add-administrator.component.ts
+++ src/app/components/add-administrator/add-administrator.component.ts
@@ -18,8 +18,8 @@ import { HttpClient } from '@angular/common/http';
 // ===========================================================================================================================
 export class AddAdministratorComponent implements OnInit {
 
-  email = new FormControl('', [Validators.required, Validators.email]);
-  baseUrl = 'http://localhost/backendAdminMailer.php';
+  email = new FormControl('', [Validators.required, Validators.email]);         // Creates the form control to validate email input
+  baseUrl = 'http://localhost/backendAdminMailer.php';                          // URL of where the php script is running
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -28,7 +28,6 @@ export class AddAdministratorComponent implements OnInit {
     );
 // ============================================ CONSTRUCTOR =================================================================
   constructor(private route: ActivatedRoute, private breakpointObserver: BreakpointObserver, private http: HttpClient) {}
-
   ngOnInit(): void {
   }
 /*===========================================================================================================================
@@ -45,8 +44,8 @@ return this.email.hasError('email') ? 'Not a valid email' : '';
 /*===========================================================================================================================
 ============================================= RESET FORM ====================================================================
 ===========================================================================================================================*/
-resetForm() {
-  if (!this.email.invalid) {
+resetForm() {                                                                   // clears the email input field only if the
+  if (!this.email.invalid) {                                                    // field has a valid email entered
   this.email.reset();
     }
   }
@@ -57,16 +56,16 @@ sendAdministrationInvite() {
   console.log(this.email.value);
 
   let postVars = {                                                              // this is created to have the email data in JSON format
-  AdminEmail : this.email.value
+  AdminEmail : this.email.value                                                 // to be posted to the php script
   };
 
   if (!this.email.invalid) {
-    this.http.post(this.baseUrl, postVars).subscribe((data) => {      // posts the data to the url which the php app is hosted
-      console.log('Got some data from backend', data);
-    }, (error) => {                                                   // gets the errors from the php app
+    this.http.post(this.baseUrl, postVars).subscribe((data) => {                // posts the data to the url which the php app is hosted
+      console.log('Got data from backend', data);                               // subscribing allows for data to be passed from php to this
+    }, (error) => {                                                             // gets the errors from the php app
       console.log('Error! ', error);
     });
-    this.resetForm();                                                 // resets the email form
+    this.resetForm();                                                           // resets the email form
   }
   }// end of Send Administration Invite
 } // end of Add Administration
