e8d9335387612493af0bab3f389b97f9700e371b
Madison Janes <madisonoliviajanes@gmail.com>
2020-04-23 06:55:37 -0400

    fixed some stuff

--- src/app/components/change-pass/change-pass.component.ts
+++ src/app/components/change-pass/change-pass.component.ts
@@ -1,41 +1,51 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder, FormGroupDirective, NgForm } from '@angular/forms';
+import { Observable, throwError } from 'rxjs';
+import {ErrorStateMatcher} from '@angular/material/core';
 
 import { Subscription } from 'rxjs';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    return control.dirty && form.invalid;
+  }
+}
 
 @Component({
   selector: 'app-change-pass',
   templateUrl: './change-pass.component.html',
   styleUrls: ['./change-pass.component.css']
 })
-export class ChangePassComponent implements OnInit, OnDestroy {
-
-  private subscription = new Subscription();
+export class ChangePassComponent {
 
   changePassForm: FormGroup;
+  oldPass = new FormControl('', [Validators.required, Validators.minLength(6)]);
+  newPass = new FormControl('', [Validators.required, Validators.minLength(6), Validators.pattern('[a-zA-Z0-9]*')]);
+  confirmPass = new FormControl('', [Validators.required, Validators.minLength(6), Validators.pattern('[a-zA-Z0-9]*')]);
 
   constructor(
     private router: Router,
-    // private loginService: LoginService,
-    private fb: FormBuilder) { }
+    private fb: FormBuilder) {
 
-  ngOnInit() {
-      this.changePassForm = this.fb.group({
-        oldPass: ['', Validators.required],
-        newPass: ['', Validators.required],
-        confirmPass: ['', Validators.required]
-  });
-}
+      this.changePassForm =
+      this.fb.group({oldPass: this.oldPass,
+          newPass: this.newPass,
+          confirmPass: this.confirmPass},
+          {
+            validator: this.passwordMatchValidator
+          });
+        }
 
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
+        matcher = new MyErrorStateMatcher();
 
-  changePass() {
-      const obj = this.changePassForm.value;
-      // this.loginService.login(obj.username, obj.password);
-  }
+        passwordMatchValidator(g: FormGroup) {
+         return g.get('newPass').value === g.get('confirmPass').value
+            ? null : { mismatch: true };
+        }
 
-}
+        changePass() {
+          // database stuff and error handling
+        }
+      }
--- src/app/components/create-survey/create-survey.component.ts
+++ src/app/components/create-survey/create-survey.component.ts
@@ -4,7 +4,6 @@ import { ActivatedRoute } from '@angular/router';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators, FormControl, FormArray } from '@angular/forms';
-import { Student } from './student';
 import { Survey, Question } from '../../services/survey';
 import { EventEmitterService } from '../event-emitter.service';
 
@@ -93,90 +92,5 @@ export class CreateSurveyComponent implements OnInit {
     }
   }
 
-  onFileChange(ev) {
-  let workBook = null;
-  let jsonData = null;
-
-  const reader = new FileReader();
-  const file = ev.target.files[0];
-
-  reader.onload = (event) => {
-    const data = reader.result;
-    workBook = XLSX.read(data, { type: 'binary' });
-    jsonData = workBook.SheetNames.reduce((initial, name) => {
-      const sheet = workBook.Sheets[name];
-      this.workSheet = workBook.Sheets[name];
-      initial[name] = XLSX.utils.sheet_to_json(sheet);
-      return initial;
-    }, {}); // end of .reduce
-
-    const dataString = JSON.stringify(jsonData);
-    // document.getElementById('output').innerHTML = dataString.slice(0, 30000).concat('...');
-
-
-    // this.IncrementCellRow(this.cellIndex);
-    // console.log(this.cellIndex);
-    this.parseNameColumn();
-    this.ParseEmailCell();
-    this.FillClassRoster();
-
-    if (this.workSheet['C12'] === undefined) { console.log('undef'); }
-    this.ParseEmailCell();
-  }; // end of onload
-  reader.readAsBinaryString(file);
-  }// end of onFileChange
-
-  IncrementCellColumn(cellIndex) {
-}// end of Incrememnt Cell Column
-
-IncrementCellRow(cellIndex) {
-console.log(this.cellIndex);
-const count = cellIndex.match(/\d*$/);
-
-this.cellIndex = this.cellIndex.substr(0, count.index) + (++count[0]);
-console.log(this.cellIndex);
-}// end of Increment Cell Row
-
-parseNameColumn() {
-var numBlankSpaces = 0;
-
-while (numBlankSpaces < 8) {
-  console.log(this.workSheet[this.cellIndex]);
-  if (this.workSheet[this.cellIndex] === undefined) {
-    numBlankSpaces++;
-  } else {
-    var cell = this.workSheet[this.cellIndex];
-    var cellData = (cell ? cell.v : undefined);
-    console.log('place the names in the queue ' + cellData );
-    console.log(cellData);
-    this.nameArr = this.nameArr + '*' + cellData; // gives a separator for the names
-    console.log(this.nameArr);
-    numBlankSpaces = 0;
-  }
-  this.IncrementCellRow(this.cellIndex);
-}
-
-this.nameArr = this.nameArr.split('*'); // splits by the separator to get an array of the names
-this.nameArr.splice(0, 1);
-console.log(this.nameArr);
-}
-
-ParseEmailCell() {
-const cell = this.workSheet['L1'];
-const cellData = (cell ? cell.v : undefined);
-this.emailArr = cellData;
-this.emailArr = this.emailArr.split(',');
-console.log(this.emailArr);
-// now add these to a class that has names and emails and concatenate them to display in the list card
-}// Parse Email Cell
-
-FillClassRoster() {
-  for (let i in this.nameArr) {
-  this.students.push(new Student(this.nameArr[i], this.emailArr[i]));
-  }
-}
-
-
-}
 // =======================================================================================================
 }// end of class
