2632320dc7d8241c8ac8c05f0f04636b6b36b98c
faithah11 <faith11hough@gmail.com>
2020-05-04 03:12:32 -0400

    added resultsservice to store the functions for getting objects

--- src/app/components/survey-results/survey-results.component.ts
+++ src/app/components/survey-results/survey-results.component.ts
@@ -1,10 +1,7 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { tap, map, shareReplay } from 'rxjs/operators';
 import { ActivatedRoute } from '@angular/router';
-import { Survey } from '../../common.types';
-import { ClassRoster } from '../../common.types';
-import { User } from '../../common.types';
-import { Results } from '../../common.types';
+import { Survey, ClassRoster, User, Results } from '../../common.types';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { Router } from '@angular/router';
@@ -13,6 +10,8 @@ import { Observable, throwError, Subscription } from 'rxjs';
 import {ErrorStateMatcher} from '@angular/material/core';
 import {MatSnackBar} from '@angular/material/snack-bar';
 import { LoginService } from '../../services/login.service';
+import { ResultsserviceService } from '../../services/Resultsservice.service';
+
 
 @Component({
   selector: 'app-survey-results',
@@ -20,37 +19,24 @@ import { LoginService } from '../../services/login.service';
   styleUrls: ['./survey-results.component.css']
 })
 export class SurveyResultsComponent implements OnInit {
-  baseUrl = 'http://localhost:8080';
-
-  constructor(private http: HttpClient) { }
+  students: ClassRoster[];
+  results: Results[];
+  constructor(private ResultsserviceService: ResultsserviceService) { }
   selectedStudent: string;
-  // this.getRosters();
-  class: User[] = [
-    {email: 'bob@email.com', name:'bob', group:'', type: ''},
-    {email: 'matt@email.com', name:'matt', group:'', type: ''}
-  ];
 
-surveys: Survey;
-modifiedText: string;
+  modifiedText: string;
 
 
   ngOnInit(): void {
+    this.ResultsserviceService.getRosters().subscribe(
+      students => {
+      if (students) {
+        this.students = students;
+      }
+    });
   }
-
 
   onStudentSelected(val:any){
-    this.onSubmit(val);
+    this.ResultsserviceService.submitStudent(val);
   }
-
-  onSubmit(val:any){
-    this.modifiedText=val;
-    console.log(JSON.stringify(this.modifiedText));
-    return this.http.post(`${this.baseUrl}/backend/api/getResults.php`, this.modifiedText).pipe(tap((response) => {console.log(response);}));
-  }
-
-  getResults(): Observable<Results[]> {
-    return this.http.get(`${this.baseUrl}/backend/api/getResults.php`).pipe(
-      map((res: any[]) => res.map(s => Results.fromJson(s))));
-  }
-
-   }
+ }
